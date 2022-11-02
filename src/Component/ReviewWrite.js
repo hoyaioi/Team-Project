0ad09a3d87be68f011af4c342185b05d3540b6dc
ggangb <rkdudrbwkddl@naver.com>
2022-11-02 22:35:01 +0900

    설문목록페이지

--- src/Component/ReviewWrite.js
+++ src/Component/ReviewWrite.js
@@ -5,9 +5,9 @@ function ReviewWrite() {
         <>
             <div className="myreviewWriteForm">
                 <form>
-                    <span>리뷰작성하기</span><br/>
+                    <h3>리뷰작성하기</h3><br/>
                     <input></input>
-                    <input></input>
+                    <textarea></textarea>
                     <input type='file'></input>
                     <input></input>
                 </form>
