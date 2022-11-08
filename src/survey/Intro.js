98d9af1535975bccaf50d019c0a990e40ccbedb9
hoyaioi <yahodaioi@gmail.com>
2022-11-08 17:23:05 +0900

    설문조사및회원정보찾기재수정

--- src/survey/Intro.js
+++ src/survey/Intro.js
@@ -1,27 +1,21 @@
-import logo from './img/logo.png';
+import logo from "./img/logo.png";
 
-
-import intro from './img/intro.png';
+import intro from "./img/intro.png";
 import "./css/Intro.css";
-import { Link, Route, Switch } from 'react-router-dom';
+import { Link, Route, Switch } from "react-router-dom";
 
 const Intro = () => {
-
-
-
   return (
     <>
-      <div className="full" >
-        
+      <div className="full">
         <img className="intro" src={intro} alt="intro" />
 
-        <a href='/surveyStart' target="_blank"><button className="start">시작하기</button></a>
+        <Link to="/surveyStart">
+          <button className="start">시작하기</button>
+        </Link>
       </div>
     </>
   );
-
 };
 
-
 export default Intro;
-
