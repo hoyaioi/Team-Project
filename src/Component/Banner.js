8a03924c3e8caafdc6cbe2bba51be0efe29a89f0
vaccine20 <rhodla@daum.net>
2022-11-16 15:35:37 +0900

    콘솔 에러원인 수정

--- src/Component/Banner.js
+++ src/Component/Banner.js
@@ -49,7 +49,7 @@ const Banner = () => {
               title="YouTube video player"
               frameborder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowfullscreen
+              allowFullScreen
             ></iframe>
           </div>
         </div>
