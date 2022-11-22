543a56e3c2a07035bd3292d3fc0652fa618a3f96
vaccine20 <rhodla@daum.net>
2022-11-22 14:13:11 +0900

    어드민 리뷰 구현, 불필요한 파일 삭제

--- src/Component/AdminMember.js
+++ src/Component/AdminMember.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import "../CSS/AdminBoard.css";
+import "../CSS/AdminContainer.css";
 
 
 const AdminMember = () => {
--- src/Component/AdminReview.js
+++ src/Component/AdminReview.js
@@ -14,46 +14,75 @@ export default function AdminReview() {
       .catch(error => console.log(error));
   }, []);
 
+  const handlerBlind = (reviewIdx) => {
+    if(window.confirm('해당 리뷰를 블라인드 처리할까요?')){
+    axios.delete(`http://localhost:8080/admin/review/remove/${reviewIdx}`)
+      .then(response => {
+        if(response.status === 200){
+          alert('블라인드 성공');
+          window.location.reload();
+        }
+      })
+      .catch(error => console.log(error));
+    }
+  }
+
+  const handlerShow = (reviewIdx) => {
+    if(window.confirm('해당 리뷰를 블라인드 해제할까요?')){
+      axios.delete(`http://localhost:8080/admin/review/show/${reviewIdx}`)
+        .then(response => {
+          if(response.status === 200){
+            alert('블라인드 해제완료');
+            window.location.reload();
+          }
+        })
+        .catch(error => console.log(error));
+      }
+  }
+
   return (
     <>
       <div id="main">
-        <div className="mypage_main_wrap">
-        <div className="adminorder_list">
-          <div className="adminorder_header">
-            <strong>관리자 리뷰목록</strong>
+        <div className="admin_container">
+          <div className="admin_title">
+            관리자 리뷰목록
           </div>
-          <table className="admin_order_table">
-            <thead >
-              <tr>
-                <th width="10%">리뷰번호</th>
-                <th width="10%">회원번호</th>
-                <th width="10%">주문번호</th>
-                <th width="30%">리뷰내용</th>
-                <th width="11%">작성일</th>
-                <th width="17%">수정일</th>
-                <th>삭제</th>
-              </tr>
-            </thead>
-            <tbody>
-
-              {datas.map((review, idx) => (
-                <tr key={idx}>
-                  <td width="10%">{review.reviewIdx}</td>
-                  <td width="10%">{review.memIdx}</td>
-                  <td width="30%">{review.orderNum}</td>
-                  <td width="10%">{review.reviewContents}</td>
-                  <td width="11%">{review.reviewWriteDate}</td>
-                  <td width="17%">{review.reviewUpdateDate}</td>
-                  <td>
-                    <div><button>삭제</button></div>
-                  </td>
+          <div className="admin_table">
+            <table>
+              <thead >
+                <tr>
+                  <td className="admin_review_idx">리뷰번호</td>
+                  <td className="admin_review_idx">회원번호</td>
+                  <td className="admin_review_ordernum">주문번호</td>
+                  <td className="admin_review_contents">리뷰내용</td>
+                  <td className="admin_review_date">작성일</td>
+                  <td className="admin_review_date">수정일</td>
+                  <td className="admin_review_blind" colSpan={2}>블라인드</td>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+              <tbody>
+                {datas.map((review, idx) => (
+                  <tr key={idx}>
+                    <td>{review.reviewIdx}</td>
+                    <td>{review.memIdx}</td>
+                    <td>{review.orderNum}</td>
+                    <td>{review.reviewContents}</td>
+                    <td>{review.reviewWriteDate}</td>
+                    <td>{review.reviewUpdateDate}</td>
+                    <td className="admin_review_blind">{review.reviewDeleteYn}</td>
+                    <td>
+                      <div className="admin_btn">
+                        <button onClick={() => handlerBlind(review.reviewIdx)}>비노출</button>
+                        <button onClick={() => handlerShow(review.reviewIdx)}>노출</button>
+                      </div>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
-        </div>
-        </div>
+      </div>
     </>
   );
 };
