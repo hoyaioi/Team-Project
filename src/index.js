23dd657e3c6188011d2306919e9c99a49ebbc87a
hoyaioi <yahodaioi@gmail.com>
2022-11-23 17:36:37 +0900

    오더 css 수정

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
