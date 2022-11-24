d6e859ad4a3c3c6b2da4535650843c84bfbab385
vaccine20 <rhodla@daum.net>
2022-11-24 12:41:27 +0900

    세션날리기

--- src/Component/MyInfoDel2.js
+++ src/Component/MyInfoDel2.js
@@ -34,6 +34,7 @@ function MyInfoDel2({memIdx}) {
                 if (response.status === 200) {
                     console.log(memIdx);
                     alert("탈퇴가 완료되었습니다. \n이용해주셔서 감사합니다.");
+                    sessionStorage.clear();
                     navigate('/');
                 } else {
                     console.log(memIdx);
