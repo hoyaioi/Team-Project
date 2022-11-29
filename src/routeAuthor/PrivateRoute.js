bd85e4407493606831b223391687504eef652e9d
vaccine20 <rhodla@daum.net>
2022-11-29 13:06:42 +0900

    얼럿 메세지 수정

--- src/routeAuthor/PrivateRoute.js
+++ src/routeAuthor/PrivateRoute.js
@@ -6,7 +6,7 @@ const PrivateRoute = ({ children }) => {
 if (isLogin) 
     return children;
   else {
-    alert("접근 권한이 없습니다 로그인 후 이용해 주세요.");
+    alert("로그인 후 이용해 주세요.");
     return <Navigate to="/login"/>;
   }
 };
